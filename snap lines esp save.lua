aeef6fe962108e0f0617abda9413e5476578d24c
Joker <110819578+jokerwtf@users.noreply.github.com>
2023-07-05 03:12:29 +0300

    Create snap lines esp save.lua

---
+++ snap lines esp save.lua
@@ -0,0 +1,22 @@
+local function ExampleSnapLines()
+    local screenCenterX, screenH = draw.GetScreenSize();
+    screenCenterX = screenCenterX * 0.5;
+
+    draw.Color(255, 0, 0, 255);
+
+    local players = entities.FindByClass("CCSPlayer");
+
+    for i = 1, #players do
+        local player = players[i];
+
+        if player:IsAlive() then
+            local x, y = client.WorldToScreen(player:GetAbsOrigin());
+
+            if x ~= nil and y ~= nil then
+                draw.Line(x, y, screenCenterX, screenH);
+            end
+        end
+    end
+end
+
+callbacks.Register("Draw", "ExampleSnapLines", ExampleSnapLines);
